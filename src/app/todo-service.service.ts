0d30d6e2d19ab2ff16e54755a84f1c373b8c5bea
vincent <vincentnacar02@gmail.com>
2018-06-21 09:19:36 +0800

    minor changes

--- src/app/todo-service.service.ts
+++ src/app/todo-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 import { Todo } from './_model/todo';
 
